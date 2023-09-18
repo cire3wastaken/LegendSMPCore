4b84185a111554ac506f35b47cfe737d33ff5105
cire3 <cire3mc@gmail.com>
2023-09-18 17:59:40 -0500

    fix breaking glitch no way !!!!!

--- src/main/java/legendsmpcore/core/GlobalConstants.java
+++ src/main/java/legendsmpcore/core/GlobalConstants.java
@@ -20,7 +20,7 @@ public final class GlobalConstants {
 
     public static final String VERSION_FILE_URL = "https://raw.githubusercontent.com/cire3wastaken/legendsmpcorever/main/version.txt";
     public static final String GITHUB_REPO = "https://github.com/cire3wastaken/LegendSmpCore/tree/master";
-    public static final String PLUGIN_VERSION = "1.0.0";
+    public static final String PLUGIN_VERSION = "1.1.0";
     public static final String OUTDATED_MESSAGE = GLOBAL_FAIL_PREFIX +
         "Legend SMP Core is not up to date! Build it yourself from " + GITHUB_REPO + " or download it from Releases!";
 }
